40ce2fa8b07ebd45e1c34193d65b12d1439f2085
Esat Yılmaz <esatcan.yilmaz@akinon.com>
2021-11-23 13:28:03 +0300

    sluglar olusturuldu

---
+++ post/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from post.models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('title', 'content', 'image', 'slug', 'created', 'modified')
--- post/api/views.py
+++ post/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.generics import ListAPIView
 from post.models import Post
+from post.api.serializers import PostSerializer
 
 
 class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
-    
-    
+    serializer_class = PostSerializer
