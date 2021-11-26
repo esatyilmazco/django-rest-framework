cade10adf1a18351ee32e18ffebcdbd28c4816b9
Esat Yılmaz <esatcan.yilmaz@akinon.com>
2021-11-26 21:17:47 +0300

    sifre degisme islemi yapildi

--- account/api/views.py
+++ account/api/views.py
@@ -1,9 +1,11 @@
 
+from rest_framework import status
 from rest_framework.generics import RetrieveUpdateAPIView, get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
-
-from account.api.serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from account.api.serializers import ChangePasswordSerializer, UserSerializer
 
 
 class ProfileView(RetrieveUpdateAPIView):
@@ -18,3 +20,29 @@ class ProfileView(RetrieveUpdateAPIView):
 
     def perform_update(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class UpdatePassword(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+    def put(self, request, *args, **kwargs):
+        self.objects = self.get_object()
+        data = {
+            "old_password": request.data["old_password"],
+            "new_password": request.data["new_password"]
+
+        }
+        serializer = ChangePasswordSerializer(data=data)
+        if serializer.is_valid():
+            old_password = serializer.data.get("old_password")
+            if not self.objects.check_password(old_password):
+                return Response({"old_password": "wrong_password"}, status=status.HTTP_400_BAD_REQUEST)
+
+            self.objects.set_password(serializer.data.get("new_password"))
+            self.objects.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
